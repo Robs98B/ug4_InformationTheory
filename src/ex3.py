72c48262dfb98234778efe527f25ce33ca044126
c-w <clemens.wolff+github@gmail.com>
2013-10-23 00:38:26 +0100

    Implement ex3

---
+++ src/ex3.py
@@ -0,0 +1,26 @@
+from math import log
+from util import Counter, bigrams
+
+
+def _main(f):
+    text = ''.join(l.strip() for l in f)
+    p_unigram = Counter(text).to_probability_distribution()
+    logprob_unigram = sum(log(p_unigram(x)) for x in text)
+    print 'unigram: nbits < %f' % (2 - logprob_unigram)
+
+    text_bigrams = list(bigrams(text))
+    p_bigram = Counter(text_bigrams).to_probability_distribution()
+    logprob_bigram = log(p_unigram(text[0]))
+    for (xm, xn) in text_bigrams:
+        logprob_bigram += log(p_bigram((xm, xn))) - log(p_unigram(xm))
+    print 'bigram:  nbits < %f' % (2 - logprob_bigram)
+
+
+def _test():
+    pass
+
+
+if __name__ == '__main__':
+    from util import DefaultOptionParser
+    from sys import modules
+    DefaultOptionParser(caller=modules[__name__], npositional=1).process_args()
